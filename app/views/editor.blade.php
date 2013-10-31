d4a08fa8cfe6d50f2199f234bf61c42aa5258a12
jeffshaver <jeff.e.shaver@gmail.com>
2013-10-31 01:13:08 +0000

    Adds login button for github and new Lato font

--- app/views/editor.blade.php
+++ app/views/editor.blade.php
@@ -2,10 +2,11 @@
 <html>
 <head>
   <link rel="stylesheet" href="css/mango.css">
-  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
 <body>
   <div id="topBar">
-    
+    <a class="button">Log in with Github</a>
   </div>
   <div id="leftMenu"></div>
   <div id="tabBar">
