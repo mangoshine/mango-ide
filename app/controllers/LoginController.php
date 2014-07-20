a006159c8ebb6bca9382b1f8e384b9673001f32d
davidyorr <davidyorr@gmail.com>
2014-07-20 22:14:07 +0000

    fixed login link

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -12,7 +12,7 @@ class LoginController extends BaseController {
 	public function getIndex()
 	{
 		if ($clientID = getenv('GH_BASIC_CLIENT_ID')) {
-			return Redirect::to('https://github.com/login/oauth/authorize?clsdfdsfient_id='.$clientID);
+			return Redirect::to('https://github.com/login/oauth/authorize?client_id='.$clientID);
 		}
 	}
 
